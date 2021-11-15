04a837f18e75e884658fa383ecd905144835f413
Charles Howard <charlesincollege7@gmail.com>
2021-11-15 17:20:07 -0500

    Added Ticket Model

---
+++ server/models/ticketModel.ts
@@ -0,0 +1,46 @@
+import { Schema, model, Document} from "mongoose";
+import {User} from "userModel";
+
+interface Ticket extends Document{
+	id: number;
+	category: string;
+	
+	state: string;
+
+	dateCreated: Date;
+	dateResolved: Date;
+		
+	contact: string;
+	tags: Array<string>
+
+	currentAssignee: User
+	caseHistory: Array<string>
+}
+
+
+const ticketSchema = new Schema<Ticket> extends User(
+	{
+	id: { type: Number, required: true},
+	category: { type: String, required: true},
+	state: { type: String, required: true},
+	
+	dateCreated: { type: Date, required: true},
+	dateResolved: { type: Date, required: false},
+	
+	contact: { type: String, required: true},
+	tags: { type: Array, required: true},
+
+	currentAssignee: { type: User, required: true}, 
+	caseHistory: { type: Array, required: false},
+
+	}
+
+);
+
+
+
+
+
+
+
+
