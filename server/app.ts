cfd58393343a003ad3997d2cfed3618629e12918
Maky <franklinjaradev@gmail.com>
2021-11-14 19:29:13 -0500

    chore: adding modules and single point

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,14 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
+
+// body parse - middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Web-Base Issue Tracking System API is running");
+});
 
 const port = 3000;
 
