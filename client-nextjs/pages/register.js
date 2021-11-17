68c5cd83c7c996f4aa21ba0264dbae5dc32b7683
Maky <franklinjaradev@gmail.com>
2021-11-16 22:57:17 -0500

    chore: adding register for client

--- client-nextjs/pages/register.js
+++ client-nextjs/pages/register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 const API_URL = "http://localhost:5000/api/users";
 
@@ -10,10 +11,46 @@ const Register = () => {
 
   const router = useRouter();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data, status } = await axios.post(`${API_URL}/register`, {
+      name,
+      email,
+      password,
+    });
+
+    if (status === 201) {
+      console.log(data);
+      router.push("/");
+    }
+  };
+
   return (
     <div>
       <h1>Register</h1>
-      <form></form>
+      <form>
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Register
+        </button>
+      </form>
     </div>
   );
 };
