2055db6aef41b90b80ba3b5c666fcdbfb7dbf3fb
Maky <franklinjaradev@gmail.com>
2021-11-17 17:59:17 -0500

    chore: adding layout for client

---
+++ client-nextjs/components/layout.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+          <li>
+            <Link href="/register">Register</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </header>
+      <main>{children}</main>
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default Layout;
--- client-nextjs/pages/index.js
+++ client-nextjs/pages/index.js
@@ -1,11 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 import React from "react";
 
 const Home = () => {
-  return <div>Hello There</div>;
+  return <div>Home Page</div>;
 };
 
 export default Home;
