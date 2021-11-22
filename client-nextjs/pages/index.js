18f13b3c417f04c7de2745327c0fada34d1fb980
Thiha Set <thihaset@mail.adelphi.edu>
2021-11-22 08:52:46 -0500

    Did styling for homepage

--- client-nextjs/pages/index.js
+++ client-nextjs/pages/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return <div className="outer">
+    <h1>Incident Tracking System - Homepage</h1>
+    <div className="aboutBloc">
+      <h1>About</h1>
+      <p>This is where we will explain the project.
+        We can explain what the project is about, what
+        we each did in the project, and possibly,
+        some of the difficulties/issues that we had.
+      </p>
+    </div>
+  </div>
+  ;
 };
 
 export default Home;
