bde928b7e7430a0ddb18b7fd9b1d1770c54a3d9e
Maky <franklinjaradev@gmail.com>
2021-11-22 20:43:06 -0500

    feature: deleteTicket and updateTicket server side

--- server/routes/ticketRoutes.ts
+++ server/routes/ticketRoutes.ts
@@ -1,6 +1,12 @@
 import express from "express";
 
-import { createTicket, getTicket } from "../controllers/ticketController";
+import {
+  createTicket,
+  deleteTicket,
+  getTicket,
+  updateTicket,
+  getUserTickets,
+} from "../controllers/ticketController";
 import { adminMiddleware, authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
@@ -8,6 +14,12 @@ const router = express.Router();
 // Only admin and authorized user is able to access ticketRoutes
 
 router.post("/ticket", authMiddleware, createTicket);
-router.get("/:id", authMiddleware, getTicket);
+router
+  .route("/:id")
+  .get(authMiddleware, getTicket)
+  .put(authMiddleware, updateTicket)
+  .delete(authMiddleware, deleteTicket);
+
+router.get("/user/:id", authMiddleware, getUserTickets);
 
 export { router as ticketRoutes };
