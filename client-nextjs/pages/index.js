885b54db8e45a8e6f167682c7d0955acce37c819
Maky <franklinjaradev@gmail.com>
2021-11-30 19:05:27 -0500

    chore: centering titles for each page

--- client-nextjs/pages/index.js
+++ client-nextjs/pages/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="outer">
-    <h1>Incident Tracking System - Homepage</h1>
-    <div className="aboutBloc">
-      <h1>About</h1>
-      <p>
-        Welcome to the Incident Tracking System. This is a System
-        used to track certain incidents across the net. You will need
-        to create an account to log or view incidents. Click on the
-        'Register' tab to create an account, if you have not done so.
-      </p>
+  return (
+    <div className="outer">
+      <h1>Incident Tracking System - Homepage</h1>
+      <div className="aboutBloc">
+        <h1>About</h1>
+        <p>
+          Welcome to the Incident Tracking System. This is a System used to
+          track certain incidents across the net. You will need to create an
+          account to log or view incidents. Click on the
+          {"'Register'"} tab to create an account, if you have not done so.
+        </p>
 
-      <hr />
+        <hr />
+      </div>
     </div>
-  </div>
-  ;
+  );
 };
 
 export default Home;
