acb1aef7bc66ca0f5f13d9a2e5ee2e086acb5450
Maky <franklinjaradev@gmail.com>
2021-12-02 16:26:59 -0500

    chore: added search/filter by category in admin page

--- client-nextjs/pages/admin/tickets/index.js
+++ client-nextjs/pages/admin/tickets/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,8 +10,21 @@ const Tickets = () => {
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const [filterCategory, setFilterCategory] = useState("");
+
   const { tickets } = useSelector((state) => state.ticket);
   const { _id, isAdmin } = useSelector((state) => state.auth.userLogin);
+  const { categories } = useSelector((state) => state.ticket);
+
+  // For Category State Management
+  let categoryArray = [];
+  for (let i = 0; i < categories.length; i++) {
+    for (let j = 0; j < categories[i].category.length; j++) {
+      categoryArray.push(categories[i].category[j]);
+    }
+  }
+
+  categoryArray.push("All");
 
   useEffect(() => {
     dispatch(getAllTickets(_id));
@@ -26,6 +39,20 @@ const Tickets = () => {
       <div className="tableContainer">
         <HeadPage title="Admin: View Tickets" />
         <h1>Tickets</h1>
+        <label>
+          Search by Category:{" "}
+          <select
+            value={filterCategory}
+            onChange={(e) => setFilterCategory(e.target.value)}
+            required
+          >
+            {categoryArray.map((item, index) => (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+        </label>
         <table>
           <thead>
             <tr>
@@ -44,22 +71,46 @@ const Tickets = () => {
           <tbody>
             {tickets.map((ticket, index) => (
               <tr key={index}>
-                <td>
-                  <Link href={`/tickets/${ticket._id}`}>{ticket._id}</Link>
-                </td>
-                <td>{ticket.category}</td>
-                <td>{ticket.description}</td>
-                <td>{ticket.dateCreated}</td>
-                <td>{ticket.dateResolved}</td>
-                <td>{ticket.state ? "True" : "False"}</td>
-                <td>{ticket.tags}</td>
-                <td>{ticket.user}</td>
-                <td>{ticket.currentAssignee}</td>
-                <td>
-                  <Link href={`/tickets/history/${ticket._id}`}>
-                    View Case History
-                  </Link>
-                </td>
+                {ticket.category === filterCategory && (
+                  <>
+                    <td>
+                      <Link href={`/tickets/${ticket._id}`}>{ticket._id}</Link>
+                    </td>
+                    <td>{ticket.category}</td>
+                    <td>{ticket.description}</td>
+                    <td>{ticket.dateCreated}</td>
+                    <td>{ticket.dateResolved}</td>
+                    <td>{ticket.state ? "True" : "False"}</td>
+                    <td>{ticket.tags}</td>
+                    <td>{ticket.user}</td>
+                    <td>{ticket.currentAssignee}</td>
+                    <td>
+                      <Link href={`/tickets/history/${ticket._id}`}>
+                        View Case History
+                      </Link>
+                    </td>
+                  </>
+                )}
+                {filterCategory === "All" && (
+                  <>
+                    <td>
+                      <Link href={`/tickets/${ticket._id}`}>{ticket._id}</Link>
+                    </td>
+                    <td>{ticket.category}</td>
+                    <td>{ticket.description}</td>
+                    <td>{ticket.dateCreated}</td>
+                    <td>{ticket.dateResolved}</td>
+                    <td>{ticket.state ? "True" : "False"}</td>
+                    <td>{ticket.tags}</td>
+                    <td>{ticket.user}</td>
+                    <td>{ticket.currentAssignee}</td>
+                    <td>
+                      <Link href={`/tickets/history/${ticket._id}`}>
+                        View Case History
+                      </Link>
+                    </td>
+                  </>
+                )}
               </tr>
             ))}
           </tbody>
