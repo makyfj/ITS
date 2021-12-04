2aaf4ac38cdfc247c4ba0c497ed488237a69976f
Maky <franklinjaradev@gmail.com>
2021-12-04 08:13:25 -0500

    fix: using react-loader-spinner instead of react-spinners

--- client-nextjs/components/spinner.js
+++ client-nextjs/components/spinner.js
@@ -1,18 +1,14 @@
-import { css } from "@emotion/react";
-import { FadeLoader } from "react-spinners";
-
-// Can be a string as well. Need to ensure each key-value pair ends with ;
-const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: blue;
-`;
+import Loader from "react-loader-spinner";
 
 const Spinner = () => {
   return (
-    <div className="sweet-loading">
-      <FadeLoader color="blue" loading={true} css={override} size={90} />
-    </div>
+    <Loader
+      type="Puff"
+      color="00bfff"
+      height={100}
+      width={100}
+      timeout={3000}
+    />
   );
 };
 
