ed2b3295412b36f1f83ecbbee1e983cb416339f4
Maky <franklinjaradev@gmail.com>
2021-12-04 01:17:37 -0500

    fix: fixing script for build

--- server/models/categoryModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose_1 = require("mongoose");
-var categorySchema = new mongoose_1.Schema({
-    category: { type: [String], required: true }
-});
-var CategoryModel = mongoose_1.model("Category", categorySchema);
-exports["default"] = CategoryModel;
--- server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var jsonwebtoken_1 = require("jsonwebtoken");
-var generateToken = function (userId) {
-    return jsonwebtoken_1["default"].sign({ userId: userId }, process.env.JWT_SECRET, {
-        expiresIn: "30d"
-    });
-};
-exports["default"] = generateToken;
